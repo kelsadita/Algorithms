87105b0bb1f3a1dd07b3ff68e9b2cb8b04554343
Kalpesh Adhatrao <kalpesh.adhatrao@gmail.com>
2013-05-03 17:08:52 +0530

    Sorting/Insertion sort

---
+++ sorting/insertion.py
@@ -0,0 +1,11 @@
+def insertion(a):
+	for j in xrange(1, len(a)):
+		key = a[j]
+		i  = j - 1
+		while i >= 0 and a[i] > key:
+			a[i + 1] = a[i]
+			a[i] = key
+			i = i - 1
+	return a
+
+print insertion([2, 3, 5, 4, 7, 8, 6])
---
+++ sorting/selection.py
@@ -0,0 +1,2 @@
+def selection(a):
+	
