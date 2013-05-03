df0afefb3546fc85d32167b417f230a8385d6e57
Kalpesh Adhatrao <kalpesh.adhatrao@gmail.com>
2013-05-03 17:43:28 +0530

    Sorting/Selection sort

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -1,7 +1,7 @@
 def insertion(a):
 	for j in xrange(1, len(a)):
 		key = a[j]
-		i  = j - 1
+		i   = j - 1
 		while i >= 0 and a[i] > key:
 			a[i + 1] = a[i]
 			a[i] = key
--- sorting/selection.py
+++ sorting/selection.py
@@ -1,2 +1,13 @@
 def selection(a):
-	
+	index = 0
+	for j in xrange(0, len(a)):
+		minq = 10000
+		for i in xrange(j, len(a)):
+			if minq > a[i]:
+				minq = a[i]
+				index = i
+		a[j], a[index] = a[index], a[j]
+		
+	return a
+
+print selection([2, 3, 5, 4, 7, 8, 6, 1, 23, 21, 19, 15])
